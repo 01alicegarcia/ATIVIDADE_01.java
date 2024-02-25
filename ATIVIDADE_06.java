88a20c1b1ab066f23fd1fdde343cd56aabc7e091
01alicegarcia <160683251+01alicegarcia@users.noreply.github.com>
2024-02-25 17:16:39 -0300

    Create ATIVIDADE_06.java
    Corrigir erros e alinhar o código

---
+++ ATIVIDADE_06.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        
+String descricaoProduto = "Fone de ouvido Bluetooth";
+int numeroProduto = 123; 
+double precoProduto = 25; 
+        
+System.out.print("Dados do Produto:");
+System.out.print("Descricao do Produto: " + descricaoProduto);
+System.out.print("Numero: " + numeroProduto);
+System.out.print("Preco: " + precoProduto);
+System.out.print("Descricao: " + descricaoProduto);
+    }
+}
