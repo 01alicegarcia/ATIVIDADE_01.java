bee336a667ae25c56f7add870a86447ed7862ed2
01alicegarcia <160683251+01alicegarcia@users.noreply.github.com>
2024-02-25 17:06:41 -0300

    Create ATIVIDADE_05.java
    Criar um sistema que apresente as informações de cada item citado, descrevendo-os.

---
+++ ATIVIDADE_05.java
@@ -0,0 +1,39 @@
+public class Produtos {
+    public static void main(String[] args) {
+
+String nomeSmartphone = "Smartphone New Vision PRO VX";
+String marcaSmartphone = "Nocturne Tech";
+double precoSmarthphone = 2500;
+int quantidadeEstoqueSmartphone = 300;
+
+
+String nomeLaptop = "Geek Seven Ultimate Version";
+String marcaLaptop = "Seven Killer";
+double precoLaptop = 4000;
+int quantidadeEstoqueLaptop = 250;
+
+String nomeFone = "Unravel New Senses";
+String marcaFone = "Inner Inspiration";
+double precoFone = 200;
+int quantidadeEstoqueFone = 150;
+
+System.out.print("Dados do Smartphone:");
+System.out.print("Nome: " + nomeSmartphone);
+System.out.print("Marca: " + marcaSmartphone);
+System.out.print("Preco: " + precoSmarthphone);
+System.out.print("Estoque: " + quantidadeEstoqueSmartphone);
+
+System.out.print("Dados do Laptop: ");
+System.out.print("Nome: " + nomeLaptop);
+System.out.print("Marca: " + marcaLaptop);
+System.out.print("Preco: "+ precoLaptop);
+System.out.print("Estoque: "+ quantidadeEstoqueLaptop);
+
+System.out.print("Dados do Fone:");
+System.out.print("Nome: " + nomeFone);
+System.out.print("Marca: " + marcaFone);
+System.out.print("Preo: " + precoFone);
+System.out.print("Estoque: "+ quantidadeEstoqueFone);
+}
+
+    }
