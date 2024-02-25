58bf777a81d4d5fc76df90f6026b1f207959f985
01alicegarcia <160683251+01alicegarcia@users.noreply.github.com>
2024-02-25 16:03:38 -0300

    Create ATIVIDADE_04.java
    Criar um sistema de controle de estoque utilizando variáveis que se encaixem com a função indicada.

---
+++ ATIVIDADE_04.java
@@ -0,0 +1,11 @@
+public class ControleEstoque {
+    public static void main(String[] args) {
+        
+String nomeDoProduto;
+String categoriaDoProduto;
+double precoDoProduto;
+int quantidadeEmEstoque;
+}
+    }
+        
+ 
