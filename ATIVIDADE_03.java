b012d826df47b063abebfc1d16af5408f1b26662
01alicegarcia <160683251+01alicegarcia@users.noreply.github.com>
2024-02-25 15:44:12 -0300

    Create ATIVIDADE_03.java
    Identificar o tipo de dado apropriado para cada variável.

---
+++ ATIVIDADE_03.java
@@ -0,0 +1,11 @@
+class HelloWorld{
+    public static void main (String[] args){
+        
+ String nomeDoAluno;
+ int idadeDoAluno;
+ float notaDaProva1;
+ float notaDaProva2;
+ double mediaFinal; 
+    }
+    
+}
