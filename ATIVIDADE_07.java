ac0b1b13e482b3c0726024367e8b7c806ce49ccd
01alicegarcia <160683251+01alicegarcia@users.noreply.github.com>
2024-02-25 17:27:08 -0300

    Create ATIVIDADE_07.java
    Organizar e alinhar o código

---
+++ ATIVIDADE_07.java
@@ -0,0 +1,16 @@
+public class ControleEstoque {
+    public static void main(String[] args) {
+     
+String nomeProduto = "Fone de Ouvido";
+String marcaProduto = "True";
+double precoProduto = 29.99;
+int quantidadeProduto = 10; 
+        
+System.out.print("Dados do Fone de Ouvido:");
+System.out.print("Nome: " + nomeProduto);
+System.out.print("Marca: " + marcaProduto);
+System.out.print("Preco: " + precoProduto);
+System.out.print("Quantidade em estoque: " + quantidadeProduto);
+    }
+}
+        
