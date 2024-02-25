3297e5642476d77c39d80f36489d8248f600272f
01alicegarcia <160683251+01alicegarcia@users.noreply.github.com>
2024-02-25 15:09:30 -0300

    Create ATIVIDADE_01.java
    Criar dez variáveis diferentes utilizando tipos de variáveis diferentes.

---
+++ ATIVIDADE_01.java
@@ -0,0 +1,15 @@
+class HelloWorld {
+    public static void main(String[] args) {
+String nome;
+char sexo;
+double altura;
+double peso;
+int idade;
+String endereco;
+long CPF;
+String cargo;
+long RG;
+float valor;
+ 
+    }
+}
