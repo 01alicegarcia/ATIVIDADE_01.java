5f2f9506d038eddbab979db423f31f0f448fff4c
01alicegarcia <160683251+01alicegarcia@users.noreply.github.com>
2024-02-25 15:33:38 -0300

    Create ATIVIDADE_02.java
    Corrigir erros conforme padrão CamelCase e incluir variáveis.

---
+++ ATIVIDADE_02.java
@@ -0,0 +1,16 @@
+class HelloWorld{
+    public static void main (String[] args){
+String nomeProduto;
+float precoProduto;
+String marcaDoProduto;
+int quantidadeProduto;
+String descricaoProduto;
+String nomeProduto;
+int numeroProduto;
+double precoDoProduto;
+float estoqueDisponivel;
+int totalDeProdutos;
+}
+
+    }
+
